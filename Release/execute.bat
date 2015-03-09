43f1c4fcb0db9a5b9e0a279be9ab8d5536097f91
Shuwei Kang <Thinkerleague@mail.fcu.edu.tw>
2015-03-09 19:03:45 +0800

    Update Ignore

---
+++ Release/execute.bat
@@ -0,0 +1,6 @@
+@echo off
+KHUI.exe kosarak_utility_spmf.txt 0 100
+KHUI.exe chess_utility_spmf.txt 0 300
+KHUI.exe chess_utility_spmf.txt 0 500
+KHUI.exe chess_utility_spmf.txt 0 700
+KHUI.exe chess_utility_spmf.txt 0 900
