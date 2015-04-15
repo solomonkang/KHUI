3dcf3fd4a109d2dde71315f825182d4aa56c0e73
Shuwei Kang <thinkerleague@mail.fcu.edu.tw>
2015-04-15 17:14:21 +0800

    Item Utility Matrix

--- KHUI/utilitylist.h
+++ KHUI/utilitylist.h
@@ -16,12 +16,16 @@ public:
 	vector<int> Itemset; //Item name
 	float Sum_IU; // Sum of Utility
 	float Sum_RU; // Sum of Remaining Utility
+	float Max_Utility;
 	vector<Element> Elements; // vector of <Element>
-	UL() : Sum_IU(0), Sum_RU(0){ //Initialize value of Sum_IU and Sum_RU
+	UL() : Sum_IU(0), Sum_RU(0), Max_Utility(0){ //Initialize value of Sum_IU and Sum_RU
 	}
 	void Add_Element(const Element& E){ //Add_Element sub function
 		Sum_IU += E.iu;
 		Sum_RU += E.ru;
 		Elements.push_back(E);
+		if (E.iu > Max_Utility){
+			Max_Utility = E.iu;
+		}
 	}
 };
