66399c48036951818d356442ad6d9581f67aa04c
Shuwei Kang <thinkerleague@mail.fcu.edu.tw>
2015-08-31 00:15:55 +0800

    For Leaving School Last Update

--- Release/execute.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-KHUI.exe kosarak_utility_khui.txt 0 1
-KHUI.exe kosarak_utility_khui.txt 0 10
-KHUI.exe kosarak_utility_khui.txt 0 100
-KHUI.exe kosarak_utility_khui.txt 0 1000
--- Release/execute_example.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-KHUI.exe Example.txt 0 2
-KHUI.exe Example.txt 0 5
-KHUI.exe Example.txt 0 10
-KHUI.exe Example.txt 0 15
-KHUI.exe Example.txt 0 20
